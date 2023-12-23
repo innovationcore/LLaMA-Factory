df821012c3f47fc2b155fc68ff2a3bebb4bbd551
codybum <cbumgardner@gmail.com>
2023-12-23 13:30:00 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -1,6 +1,6 @@
 import time
 
-from peft import PeftModel
+from peft import PeftModel, PeftConfig
 
 from llmtuner import AdvancedEvaluator
 
@@ -25,15 +25,17 @@ def main():
     adapter_to_merge = ['/workspace/models/adapters/uk-med-text-v1','/workspace/models/adapters/medal-v1']
 
     time_stamp = time.time()
+
     model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="default")
 
+    model.set_adapter("default")
     print('load base adapter time:', time.time() - time_stamp)
-    model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
-    print('load second adapter:', time.time() - time_stamp)
-    model.add_weighted_adapter(adapters=['default', 'medal-v1'], weights=[0.5, 0.5], adapter_name="combined",combination_type="linear")
-    print('add_weights:', time.time() - time_stamp)
-    model.set_adapter("combined")
-    print('merge time:', time.time() - time_stamp)
+    #model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
+    #print('load second adapter:', time.time() - time_stamp)
+    #model.add_weighted_adapter(adapters=['default', 'medal-v1'], weights=[0.5, 0.5], adapter_name="combined",combination_type="linear")
+    #print('add_weights:', time.time() - time_stamp)
+    #model.set_adapter("combined")
+    #print('merge time:', time.time() - time_stamp)
 
     advanced_evaluator.set_model(model)
 
