abe23de10e2f71117ac4393b7eea6cedd49efb91
codybum <cbumgardner@gmail.com>
2023-12-23 14:48:37 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -25,28 +25,23 @@ def main():
     #combined_results['base'] = get_score(category_corrects)
 
     #do new adapters
-    adapter_to_merge = ['/workspace/models/adapters/uk-med-text-v1','/workspace/models/adapters/medal-v1']
+    # {'base': {'Average': 72.4, 'STEP-1': 67.06, 'STEP-2': 78.16, 'STEP-3': 71.96}, 'config_1': {'Average': 70.25, 'STEP-1': 67.06, 'STEP-2': 77.01, 'STEP-3': 67.29}}
+    adapter_to_merge = ['/workspace/models/adapters/medal-v1','/workspace/models/adapters/uk-med-text-v1']
 
-    time_stamp = time.time()
+    model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="medal-v1")
+    model.set_adapter("medal-v1")
+    model.eval()
+    advanced_evaluator.set_model(model)
+    category_corrects, results = advanced_evaluator.eval()
+    combined_results['medal-v1'] = get_score(category_corrects)
 
-    model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="default")
-    model.delete_adapter('default')
-    #{'base': {'Average': 72.4, 'STEP-1': 67.06, 'STEP-2': 78.16, 'STEP-3': 71.96}, 'config_1': {'Average': 70.25, 'STEP-1': 67.06, 'STEP-2': 77.01, 'STEP-3': 67.29}}
-    '''
-    print('load base adapter time:', time.time() - time_stamp)
-    model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
-    print('load second adapter:', time.time() - time_stamp)
-    model.add_weighted_adapter(adapters=['default', 'medal-v1'], weights=[0.1, 0.9], adapter_name="combined",combination_type="linear")
-    print('add_weights:', time.time() - time_stamp)
-    print('merge time:', time.time() - time_stamp)
-
+    model.load_adapter(adapter_to_merge[1], adapter_name="uk-med-text-v1")
+    model.add_weighted_adapter(adapters=['medal-v1', 'uk-med-text-v1'], weights=[0.5, 0.5], adapter_name="combined",combination_type="linear")
     model.set_adapter("combined")
     model.eval()
-    '''
     advanced_evaluator.set_model(model)
-
     category_corrects, results = advanced_evaluator.eval()
-    combined_results['config_1'] = get_score(category_corrects)
+    combined_results['medal-v1_uk-med-text-v1'] = get_score(category_corrects)
 
     print(combined_results)
 
