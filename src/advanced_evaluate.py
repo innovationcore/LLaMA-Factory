275a4614d5f8558bd76b8d865ed497727b87a23a
codybum <cbumgardner@gmail.com>
2023-12-23 13:49:17 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -28,15 +28,15 @@ def main():
 
     model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="default")
 
-    model.set_adapter("default")
     print('load base adapter time:', time.time() - time_stamp)
-    #model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
-    #print('load second adapter:', time.time() - time_stamp)
-    #model.add_weighted_adapter(adapters=['default', 'medal-v1'], weights=[0.5, 0.5], adapter_name="combined",combination_type="linear")
-    #print('add_weights:', time.time() - time_stamp)
-    #model.set_adapter("combined")
-    #print('merge time:', time.time() - time_stamp)
+    model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
+    print('load second adapter:', time.time() - time_stamp)
+    model.add_weighted_adapter(adapters=['default', 'medal-v1'], weights=[0.5, 0.5], adapter_name="combined",combination_type="linear")
+    print('add_weights:', time.time() - time_stamp)
+    print('merge time:', time.time() - time_stamp)
 
+    model.set_adapter("combined")
+    model.eval()
     advanced_evaluator.set_model(model)
 
     advanced_evaluator.eval()
