b24bab92589620ca1ab0a26f472529022c72b98b
codybum <cbumgardner@gmail.com>
2023-12-23 15:21:56 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -25,15 +25,14 @@ def main():
     category_corrects, results = advanced_evaluator.eval()
     combined_results['base'] = get_score(category_corrects)
     advanced_evaluator.unload_model()
-    time.sleep(15)
 
-    exit(0)
     #do new adapters
     # {'base': {'Average': 72.4, 'STEP-1': 67.06, 'STEP-2': 78.16, 'STEP-3': 71.96}, 'config_1': {'Average': 70.25, 'STEP-1': 67.06, 'STEP-2': 77.01, 'STEP-3': 67.29}}
     # {'medal-v1': {'Average': 73.84, 'STEP-1': 71.76, 'STEP-2': 77.01, 'STEP-3': 72.9}, 'medal-v1_uk-med-text-v1': {'Average': 69.53, 'STEP-1': 68.24, 'STEP-2': 74.71, 'STEP-3': 66.36}}
     # {'medal-v1': {'Average': 73.84, 'STEP-1': 71.76, 'STEP-2': 77.01, 'STEP-3': 72.9}, 'medal-v1_uk-med-text-v1': {'Average': 70.61, 'STEP-1': 70.59, 'STEP-2': 73.56, 'STEP-3': 68.22}}
     adapter_to_merge = ['/workspace/models/adapters/medal-v1','/workspace/models/adapters/uk-med-text-v1']
 
+    advanced_evaluator.load_model()
     model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="medal-v1")
     model.set_adapter("medal-v1")
     #model.requires_grad_(False)
@@ -41,7 +40,9 @@ def main():
     advanced_evaluator.set_model(model)
     category_corrects, results = advanced_evaluator.eval()
     combined_results['medal-v1'] = get_score(category_corrects)
+    advanced_evaluator.unload_model()
 
+    advanced_evaluator.load_model()
     model.load_adapter(adapter_to_merge[1], adapter_name="uk-med-text-v1")
     model.add_weighted_adapter(adapters=['medal-v1', 'uk-med-text-v1'], weights=[5.0, 5.0], adapter_name="combined",combination_type="linear")
     model.set_adapter("combined")
@@ -50,7 +51,8 @@ def main():
     advanced_evaluator.set_model(model)
     category_corrects, results = advanced_evaluator.eval()
     combined_results['medal-v1_uk-med-text-v1'] = get_score(category_corrects)
-
+    advanced_evaluator.unload_model()
+    
     print(combined_results)
 
 if __name__ == "__main__":
