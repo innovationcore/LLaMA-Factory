d601942d23885ff84a56894e1607c8648ff3991f
codybum <cbumgardner@gmail.com>
2023-12-23 16:03:52 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -32,19 +32,20 @@ def main():
     # {'medal-v1': {'Average': 73.84, 'STEP-1': 71.76, 'STEP-2': 77.01, 'STEP-3': 72.9}, 'medal-v1_uk-med-text-v1': {'Average': 69.53, 'STEP-1': 68.24, 'STEP-2': 74.71, 'STEP-3': 66.36}}
     # {'medal-v1': {'Average': 73.84, 'STEP-1': 71.76, 'STEP-2': 77.01, 'STEP-3': 72.9}, 'medal-v1_uk-med-text-v1': {'Average': 70.61, 'STEP-1': 70.59, 'STEP-2': 73.56, 'STEP-3': 68.22}}
     #{'base': {'Average': 73.84, 'STEP-1': 70.59, 'STEP-2': 78.16, 'STEP-3': 72.9}, 'medal-v1': {'Average': 70.25, 'STEP-1': 69.41, 'STEP-2': 73.56, 'STEP-3': 68.22}, 'medal-v1_uk-med-text-v1': {'Average': 59.86, 'STEP-1': 58.82, 'STEP-2': 68.97, 'STEP-3': 53.27}}
-    adapter_to_merge = ['/workspace/models/adapters/medal-v1','/workspace/models/adapters/uk-med-text-v1']
+    adapter_to_merge = ['/workspace/models/adapters/uk-med-text-v1', '/workspace/models/adapters/medal-v1']
 
     advanced_evaluator.load_model()
-    model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="medal-v1")
+    model = PeftModel.from_pretrained(advanced_evaluator.get_model(), adapter_to_merge[0], adapter_name="default")
     print(model.active_adapters, model.active_adapter)
-    model.set_adapter("medal-v1")
+    model.load_adapter(adapter_to_merge[0], adapter_name="uk-med-text-v1")
+    #model.set_adapter("medal-v1")
     print(model.active_adapters, model.active_adapter)
-    model.load_adapter(adapter_to_merge[1], adapter_name="uk-med-text-v1")
+    model.load_adapter(adapter_to_merge[1], adapter_name="medal-v1")
     print(model.active_adapters, model.active_adapter)
     model.add_weighted_adapter(adapters=['medal-v1', 'uk-med-text-v1'], weights=[5.0, 5.0], adapter_name="combined",
                                combination_type="linear")
     print(model.active_adapters, model.active_adapter)
-
+    model.set_adapter("combined")
     exit(0)
     #model.requires_grad_(False)
     #model.eval()
