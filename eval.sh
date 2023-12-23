fcb81b7b442279c1b6899dbdfc889971c50c4e35
codybum <cbumgardner@gmail.com>
2023-12-23 09:44:41 -0500

    add dataset

--- eval.sh
+++ eval.sh
@@ -3,9 +3,11 @@
 #--adapter_name_or_path /workspace/models/adapters/1 \
 #--adapter_name_or_path /workspace/models/adapters/med-text-pt \
 
-export ADAPTER=med-text-pt-256
+export ADAPTER=uk-med-text-v1,uk-med-text-v2,uk-med-text-v3
 
 rm -rf /root/.cache/huggingface/datasets/
+
+start=$(date +%s)
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python src/evaluate.py \
     --model_name_or_path /workspace/models/Mixtral-8x7B-Instruct-v0.1 \
@@ -17,3 +19,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python src/evaluate.py \
     --lang en \
     --n_shot 5 \
     --batch_size 2
+
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
