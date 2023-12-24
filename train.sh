4fb72a0649e6e203bf010833044592cd3fae92de
codybum <cbumgardner@gmail.com>
2023-12-24 10:26:56 -0500

    add dataset

--- train.sh
+++ train.sh
@@ -19,26 +19,35 @@ echo "NCCL_PROTO="$NCCL_PROTO
 echo "LD_PRELOAD="$LD_PRELOAD
 echo "NCCL_DEBUG="$NCCL_DEBUG
 
-export LORA_RANK=8
-export LORA_ALPHA=8
-#export LORA_TARGET=all
+#dont timeout
+#export NCCL_BLOCKING_WAIT=1
+#export NCCL_IB_GID_INDEX=3
+#export NCCL_P2P_LEVEL=NVL
+# to keep long runs from dying
+#export NCCL_TIMEOUT=14400000
+#export NCCL_IB_TIMEOUT=22
+export DDP_TIMEOUT=14400
+
+export LORA_RANK=64
+export LORA_ALPHA=64
+export LORA_TARGET=all
 #export LORA_TARGET=q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj
 #export LORA_TARGET=k_proj,w2,o_proj,q_proj,w1,w3,gate,v_proj
-export LORA_TARGET=q_proj,v_proj
+#export LORA_TARGET=q_proj,v_proj
 
 echo "LORA_RANK="$LORA_RANK
 echo "LORA_RANK="$LORA_ALPHA
 echo "LORA_TARGET="$LORA_TARGET
 
-export BATCH_SIZE=32
+export BATCH_SIZE=48
 echo "BATCH_SIZE="$BATCH_SIZE
 
 export GRADIENT_ACCUMULATION_STEPS=4
 echo "GRADIENT_ACCUMULATION_STEPS="$GRADIENT_ACCUMULATION_STEPS
 
-export EPOCH=1.0
-#export LR=5e-5
-export LR=1e-6
+export EPOCH=3.0
+export LR=5e-5
+#export LR=1e-6
 
 #export TEMPLATE=default
 export TEMPLATE=mistral
@@ -47,16 +56,16 @@ export TEMPLATE=mistral
 #export MODEL=/workspace/basemodels/llama-2-70b-chat-hf
 #export MODEL=/workspace/basemodels/falcon-180B
 export MODEL=/workspace/basemodels/Mixtral-8x7B-Instruct-v0.1
-#export ADAPTER=/workspace/basemodels/Mixtral-8x7B-Instruct-v0.1_adapters/med-text-pt 
+#export ADAPTER=/workspace/basemodels/Mixtral-8x7B-Instruct-v0.1_adapters/med-text-pt
 #export ADAPTER=/workspace/outputmodels/med-text-128
 
-export STAGE=pt
-#export STAGE=sft
+#export STAGE=pt
+export STAGE=sft
 
 #export DATASET=c4_demo
 #export DATASET=wiki_demo
-export DATASET=uk-med-text
-#export DATASET=uk-data-train
+#export DATASET=uk-med-text
+export DATASET=uk-data-train
 #export DATASET=medal_full
 
 cd /workspace
@@ -72,6 +81,7 @@ accelerate launch --num_processes=$(( 8 * $WORLD_SIZE )) --num_machines $WORLD_S
     --dataset $DATASET \
     --dataset_dir /workspace/data \
     --template $TEMPLATE \
+    --ddp_timeout $DDP_TIMEOUT \
     --finetuning_type lora \
     --lora_rank $LORA_RANK \
     --lora_alpha $LORA_ALPHA \
