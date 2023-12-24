ba52911b64d58e40da463f5f966237fe2311e30e
codybum <cbumgardner@gmail.com>
2023-12-24 09:41:20 -0500

    add dataset

--- src/advanced_evaluate.py
+++ src/advanced_evaluate.py
@@ -30,7 +30,7 @@ def main():
     model = PeftModel.from_pretrained(advanced_evaluator.get_model(), peft_model_id)
     model.load_adapter(adapter_to_merge[0], adapter_name="medal-v1")
     model.load_adapter(adapter_to_merge[1], adapter_name="uk-med-text-v1")
-    model.add_weighted_adapter(adapters=['medal-v1', 'uk-med-text-v1'], weights=[1.0, 1.0], adapter_name="combined", combination_type="linear")
+    model.add_weighted_adapter(adapters=['medal-v1', 'uk-med-text-v1'], weights=[0.25, 0.25], adapter_name="combined", combination_type="linear")
     model.set_adapter("combined")
     model.merge_adapter(['combined'])
 
