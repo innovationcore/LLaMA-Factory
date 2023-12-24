8adee0ed334434c69eb53cc2e9b77fe19174f825
codybum <cbumgardner@gmail.com>
2023-12-24 08:45:19 -0500

    add dataset

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #--adapter_name_or_path /workspace/models/adapters/med-text-pt \
+#--template vanilla \
 
 rm -rf /root/.cache/huggingface/datasets/
 
@@ -8,7 +9,7 @@ start=$(date +%s)
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python src/advanced_evaluate.py \
     --model_name_or_path /workspace/basemodels/Mixtral-8x7B-Instruct-v0.1 \
-    --template vanilla \
+    --template mistral \
     --finetuning_type lora \
     --task usmle \
     --split test \
