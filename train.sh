046b6fc9b5775c912ae1736995f66c9e0d1eb0d4
codybum <cbumgardner@gmail.com>
2023-12-26 16:30:25 -0500

    add dataset

--- train.sh
+++ train.sh
@@ -71,7 +71,8 @@ export STAGE=sft
 #export DATASET=uk-med-text
 #export DATASET=uk-data-train
 #export DATASET=medal_full
-export DATASET=case-chat-med-train
+#export DATASET=case-chat-med-train
+export DATASET=qa-med-train
 
 #echo $DATASET'_S-'$STAGE'_R-'$LORA_RANK'_A-'$LORA_ALPHA'_E-'$EPOCH'_LR-'$LR
 #echo $DATASET _S- $STAGE _R- $LORA_RANK _A-\ $LORA_ALPHA _E- $EPOCH _LR- $LR
