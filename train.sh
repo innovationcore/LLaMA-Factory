0b4b6bd4d48d5f137f4ae85046ccd47fd29b2cf0
codybum <cbumgardner@gmail.com>
2023-12-27 12:23:29 -0500

    fized dataset

--- train.sh
+++ train.sh
@@ -28,8 +28,8 @@ echo "NCCL_DEBUG="$NCCL_DEBUG
 #export NCCL_IB_TIMEOUT=22
 export DDP_TIMEOUT=14400
 
-export LORA_RANK=8
-export LORA_ALPHA=8
+#export LORA_RANK=8
+#export LORA_ALPHA=8
 
 #export LORA_TARGET=all
 export LORA_TARGET=q_proj,v_proj
@@ -64,16 +64,16 @@ export MODEL=/workspace/basemodels/Mixtral-8x7B-Instruct-v0.1
 #export ADAPTER=/workspace/basemodels/Mixtral-8x7B-Instruct-v0.1_adapters/med-text-pt
 #export ADAPTER=/workspace/outputmodels/med-text-128
 
-export STAGE=pt
-#export STAGE=sft
+#export STAGE=pt
+export STAGE=sft
 
 #export DATASET=c4_demo
 #export DATASET=wiki_demo
-export DATASET=uk-med-text
+#export DATASET=uk-med-text
 #export DATASET=uk-data-train
 #export DATASET=medal_full
 #export DATASET=case-chat-med-train
-#export DATASET=qa-med-train
+export DATASET=qa-med-train
 #export DATASET=multi-choice-med-train
 
 #echo $DATASET'_S-'$STAGE'_R-'$LORA_RANK'_A-'$LORA_ALPHA'_E-'$EPOCH'_LR-'$LR
@@ -85,29 +85,35 @@ cd /workspace
 
 #--adapter_name_or_path $ADAPTER \
 
-accelerate launch --num_processes=$(( 8 * $WORLD_SIZE )) --num_machines $WORLD_SIZE  --machine_rank $RANK --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT --config_file=/workspace/config/accelerate_config.yaml \
-    /workspace/src/train_bash.py \
-    --stage $STAGE \
-    --model_name_or_path $MODEL \
-    --do_train \
-    --flash_attn \
-    --dataset $DATASET \
-    --dataset_dir /workspace/data \
-    --template $TEMPLATE \
-    --ddp_timeout $DDP_TIMEOUT \
-    --finetuning_type lora \
-    --lora_rank $LORA_RANK \
-    --lora_alpha $LORA_ALPHA \
-    --lora_target $LORA_TARGET \
-    --output_dir /workspace/outputmodels/$OUTPUT_MODEL \
-    --overwrite_output_dir \
-    --overwrite_cache \
-    --per_device_train_batch_size $BATCH_SIZE \
-    --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
-    --lr_scheduler_type cosine \
-    --logging_steps 1 \
-    --save_steps 1000 \
-    --learning_rate $LR \
-    --num_train_epochs $EPOCH \
-    --plot_loss \
-    --bf16
+for rank in 8 16 32 128 256
+do
+  export LORA_RANK=rank
+  export LORA_ALPHA=rank
+
+  accelerate launch --num_processes=$(( 8 * $WORLD_SIZE )) --num_machines $WORLD_SIZE  --machine_rank $RANK --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT --config_file=/workspace/config/accelerate_config.yaml \
+      /workspace/src/train_bash.py \
+      --stage $STAGE \
+      --model_name_or_path $MODEL \
+      --do_train \
+      --flash_attn \
+      --dataset $DATASET \
+      --dataset_dir /workspace/data \
+      --template $TEMPLATE \
+      --ddp_timeout $DDP_TIMEOUT \
+      --finetuning_type lora \
+      --lora_rank $LORA_RANK \
+      --lora_alpha $LORA_ALPHA \
+      --lora_target $LORA_TARGET \
+      --output_dir /workspace/outputmodels/$OUTPUT_MODEL \
+      --overwrite_output_dir \
+      --overwrite_cache \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
+      --lr_scheduler_type cosine \
+      --logging_steps 1 \
+      --save_steps 1000 \
+      --learning_rate $LR \
+      --num_train_epochs $EPOCH \
+      --plot_loss \
+      --bf16
+done
