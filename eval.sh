25310e98aa0a2c4b9c33a96eacee7a9552081ebf
codybum <cbumgardner@gmail.com>
2023-12-28 14:37:22 -0500

    splitzville

--- eval.sh
+++ eval.sh
@@ -12,11 +12,11 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python src/advanced_evaluate.py \
     --model_name_or_path /workspace/basemodels/Mixtral-8x7B-Instruct-v0.1 \
     --template vanilla \
     --finetuning_type lora \
-    --task usmle \
+    --task mausmle \
     --split test \
     --lang en \
     --n_shot 5 \
-    --batch_size 8
+    --batch_size 6
 
 end=$(date +%s)
 echo "Elapsed Time: $(($end-$start)) seconds"
