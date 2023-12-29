8c767280ef72f167d706d2a0b81cae69664c6fad
codybum <cbumgardner@gmail.com>
2023-12-28 19:32:55 -0500

    splitzville

--- src/optimize_adapters.py
+++ src/optimize_adapters.py
@@ -79,6 +79,12 @@ def run_inf(inf_config):
 
 def objective(trial):
 
+    #{'base': {'Average': 60.54, 'MEDICINE': 56.52, 'OPHTHALMOLOGY': 59.52, 'ANATOMY': 63.01, 'PATHOLOGY': 70.54,
+    # 'PHYSIOLOGY': 65.91, 'DENTAL': 51.78, 'RADIOLOGY': 66.07, 'BIOCHEMISTRY': 74.38, 'ANAESTHESIA': 73.91,
+    # 'GYNAECOLOGY': 55.56, 'PHARMACOLOGY': 73.03, 'SOCIAL': 51.11, 'PEDIATRICS': 59.09, 'ENT': 73.68,
+    # 'SURGERY': 62.5, 'MICROBIOLOGY': 61.64, 'FORENSIC': 69.77, 'PSYCHIATRY': 77.78, 'SKIN': 60.0,
+    # 'ORTHOPAEDICS': 71.43, 'UNKNOWN': 100.0}}
+
     disable_adapters = False
 
     inf_config = dict()
@@ -86,31 +92,66 @@ def objective(trial):
     adapters_path = '/workspace/models/adapters/'
     inf_config['adapters_path'] = adapters_path
 
+    candiate_adapters = dict()
 
+    candiate_adapters['case-chat-med-train'] = dict()
+    candiate_adapters['case-chat-med-train']['epoch'] = [1]
+    candiate_adapters['case-chat-med-train']['lr'] = ['5e-5']
+    candiate_adapters['case-chat-med-train']['rank'] = [8, 16, 32, 64, 128, 256]
+    candiate_adapters['case-chat-med-train']['stage'] = 'sft'
+
+    candiate_adapters['qa-med-train'] = dict()
+    candiate_adapters['qa-med-train']['epoch'] = [1]
+    candiate_adapters['qa-med-train']['lr'] = ['5e-5']
+    candiate_adapters['qa-med-train']['rank'] = [8, 16, 32, 64, 128, 256]
+    candiate_adapters['qa-med-train']['stage'] = 'sft'
+
+    candiate_adapters['medqa-textbooks-dataset'] = dict()
+    candiate_adapters['medqa-textbooks-dataset']['epoch'] = [1]
+    candiate_adapters['medqa-textbooks-dataset']['lr'] = ['5e-5']
+    candiate_adapters['medqa-textbooks-dataset']['rank'] = [8, 16, 32, 64, 128, 256]
+    candiate_adapters['medqa-textbooks-dataset']['stage'] = 'pt'
+
+    '''
     lora_rank = trial.suggest_categorical('lora_rank', [8, 16, 32, 64, 128, 256])
     candiate_adapters = ['case-chat-med-train_S-sft_R-' + str(lora_rank) + '_A-' + str(lora_rank) + '_E-1_LR-5e-5',
                          'qa-med-train_S-sft_R-' + str(lora_rank) + '_A-' + str(lora_rank) + '_E-1_LR-5e-5',
                          'medqa-textbooks-dataset_S-pt_R-' + str(lora_rank) + '_A-' + str(lora_rank) + '_E-1_LR-5e-5']
     inf_config['lora_rank'] = lora_rank
+    '''
 
     adapter_config = dict()
 
     need_adapter = True
 
-    for adapter in candiate_adapters:
-        adapter_config[adapter] = dict()
-        adapter_is_enabled_id = adapter + '_is_enabled'
+    for adapter_name, adapter_info in candiate_adapters:
+        adapter_config[adapter_name] = dict()
+
+        # set rank/alpha
+        rank = trial.suggest_categorical(adapter_name + '_rank', adapter_info['rank'])
+        epoch = trial.suggest_categorical(adapter_name + '_epoch', adapter_info['epoch'])
+        lr = trial.suggest_categorical(adapter_name + '_lr', adapter_info['lr'])
+        stage = adapter_info['stage']
+
+        adapter_id = adapter_name + '_S-' + stage + '_R-' + str(rank) + '_A-' + str(rank) + '_E-' + str(epoch) + '_LR-' + lr
+
+        adapter_is_enabled_id = adapter_id + '_is_enabled'
         if disable_adapters:
             is_enabled = trial.suggest_categorical(adapter_is_enabled_id, [True, False])
         else:
             is_enabled = True
-        adapter_config[adapter]['is_enabled'] = is_enabled
+
+        adapter_config[adapter_id]['is_enabled'] = is_enabled
         if is_enabled:
             need_adapter = False
-            adapter_weight = adapter + '_weight'
-            adapter_config[adapter]['weight'] = trial.suggest_float(adapter_weight, 0.0, 1.25, step=0.25)
+            #set weights
+            adapter_weight = adapter_id + '_weight'
+            adapter_config[adapter_id]['weight'] = trial.suggest_float(adapter_weight, 0.0, 1.25, step=0.25)
+
 
     if need_adapter:
+        print('NEED AN ADAPTER NEED TO FIX THIS')
+        exit()
         print('NO ADAPTER SELECTED: PICKING RANDOM')
         adapter = random.choice(list(adapter_config))
         print('RANDOM:', adapter)
