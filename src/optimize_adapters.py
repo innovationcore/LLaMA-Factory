ff9ed96883d1d0844d7a6ea7fe931869810c92a1
codybum <cbumgardner@gmail.com>
2023-12-30 19:39:51 -0500

    bringin it home

--- src/optimize_adapters.py
+++ src/optimize_adapters.py
@@ -144,6 +144,13 @@ def objective(trial):
         candiate_adapters['qa-med-train']['rank'] = [8, 16, 32, 64, 128, 256]
         candiate_adapters['qa-med-train']['stage'] = ['sft']
 
+        candiate_adapters['medqa-textbooks-dataset'] = dict()
+        candiate_adapters['medqa-textbooks-dataset']['model'] = ['llama-2-7b-chat-hf']
+        candiate_adapters['medqa-textbooks-dataset']['epoch'] = [1]
+        candiate_adapters['medqa-textbooks-dataset']['lr'] = ['5e-5']
+        candiate_adapters['medqa-textbooks-dataset']['rank'] = [8, 16, 32, 64, 128, 256]
+        candiate_adapters['medqa-textbooks-dataset']['stage'] = ['pt']
+
         candiate_adapters['multi-choice-med-train'] = dict()
         candiate_adapters['multi-choice-med-train']['model'] = ['llama-2-7b-chat-hf']
         candiate_adapters['multi-choice-med-train']['epoch'] = [1]
