840667091a6df4c8641354ac965f9b95916b1c3f
codybum <cbumgardner@gmail.com>
2024-01-09 14:12:34 -0500

    blam

--- optimize.sh
+++ optimize.sh
@@ -7,7 +7,8 @@
 #export MODEL=llama-2-7b-chat-hf
 #export MODEL=Mixtral-8x7B-Instruct-v0.1
 #export MODEL=MELT-Mistral-3x7B-Instruct-v0.1
-export MODEL=llama-2-3x70b-chat-hf
+#export MODEL=llama-2-3x70b-chat-hf
+export MODEL=TinyLlama-16x1.1B-Chat-v1.0
 
 #export TEMPLATE=mistral
 export TEMPLATE=vanilla
@@ -26,7 +27,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python /workspace/src/optimize_adapters.py 
     --split test \
     --lang en \
     --n_shot 5 \
-    --batch_size 2
+    --batch_size 8
 
 end=$(date +%s)
 echo "Elapsed Time: $(($end-$start)) seconds"
