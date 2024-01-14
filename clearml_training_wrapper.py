d706d89921e33104c11a63f34caed5a08c1612c1
codybum <cbumgardner@gmail.com>
2024-01-14 12:31:33 -0500

    blam

--- clearml_training_wrapper.py
+++ clearml_training_wrapper.py
@@ -10,6 +10,8 @@ import pandas as pd
 from clearml import Task
 from clearml import Logger
 
+step_count = 1
+
 def extract_string_between_curly_braces(text):
 
   match = re.search(r'\{(.*?)\}', text)
@@ -50,10 +52,14 @@ def execute(cmd, stdout_cb, stderr_cb):
 
 def upload_training_stats(training_stats):
 
+    global step_count  # declare a to be a global
+
     print('upload_training_stats:', training_stats)
-    Logger.current_logger().report_scalar("STEP_EPOCH", "step_epoch", iteration=None, value=training_stats['epoch'])
-    Logger.current_logger().report_scalar("LOSS", "loss", iteration=None, value=training_stats['loss'])
-    Logger.current_logger().report_scalar("LR", "lr", iteration=None, value=training_stats['learning_rate'])
+    Logger.current_logger().report_scalar("STEP_EPOCH", "step_epoch", iteration=step_count, value=training_stats['epoch'])
+    Logger.current_logger().report_scalar("LOSS", "loss", iteration=step_count, value=training_stats['loss'])
+    Logger.current_logger().report_scalar("LR", "lr", iteration=step_count, value=training_stats['learning_rate'])
+
+    step_count += 1
 
 def update_training_metrics(metric_key, metric_value):
     print('update_training_metrics: UPLOAD TRAINING METRIC:','metric_key:', metric_key, 'metric_value:', metric_value)
