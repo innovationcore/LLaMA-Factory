8cf131f7290c4b7627c98d42ebe94b2cec863e5d
Cody Bumgardner <cody@uky.edu>
2024-06-27 20:14:23 -0400

    updates due to paths

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,8 @@
 export MAX_JOBS=40
 
 docker build -f ./Dockerfile_llmfactory \
-    --build-arg INSTALL_BNB=true \
-    --build-arg INSTALL_VLLM=true \
+    --build-arg INSTALL_BNB=false \
+    --build-arg INSTALL_VLLM=false \
     --build-arg INSTALL_DEEPSPEED=true \
     --build-arg INSTALL_FLASHATTN=true \
     --build-arg PIP_INDEX=https://pypi.org/simple \
